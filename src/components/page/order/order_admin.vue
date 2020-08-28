962ed57dc3c95c19c81bd911df4a1662ef6ad44c
username <user@1320604966@qq.com>
2020-08-28 16:34:39 +0800

    fjs

--- src/components/page/order/order_admin.vue
+++ src/components/page/order/order_admin.vue
@@ -319,7 +319,7 @@
 				console.log(row)
 				var input =  document.createElement('input')
 				input.type = 'file'
-				input.accept = '.docx'
+				input.accept = '.docx,.DOCX,.doc,.DOC,.pdf,.PDF'
 				input.addEventListener('change',(event)=>{
 					let file = event.target.files[0]
 					var data = new FormData()
