06f9b9e82a97d7162cccb8140f362eb22b68f1d5
nmt551379 <1103524062@qq.com>
2020-09-10 18:48:30 +0800

    add

--- src/components/page/examination/examination_process.vue
+++ src/components/page/examination/examination_process.vue
@@ -37,24 +37,39 @@
 				<div class="go-box">
 					<div class="go">
 						<div class="ts">我有打印机</div>
-						<el-button class="buttom">使用答题卡辅助作答</el-button>
+						<el-button class="buttom" @click="goScantronHas()">使用答题卡辅助作答</el-button>
 						<div class="ts1">
 							<p>1、进入系统下载答题卡</p>
 							<p>2、打印答题卡</p>
 							<p>3、在答题卡上作答</p>
 							<p>4、作答完毕后逐题拍照上传答案</p>
-							<p>5、交卷</p>                
+							<p>5、交卷</p>
 						</div>
 					</div>
 					<div class="go">
 						<div class="ts">在线试卷</div>
-						<el-button class="buttom">人脸录入</el-button>
+						<el-button class="buttom" @click="goFaceRecognition()">人脸录入</el-button>
 						<div class="ts1">
 							<p>1.进入考试前必须进行人脸录入</p>
 						</div>
 					</div>
-					<div class="go"></div>
-					<div class="go"></div>
+					<div class="go">
+						<div class="ts">我没有办法打印</div>
+						<el-button class="buttom" @click="goScantronNone()">在线考试辅助</el-button>
+						<div class="ts1">
+							<p>1、进入在线考试系统</p>
+							<p>2、在线完成选择题的作答</p>
+							<p>3、扫码上传其他题的作答</p>
+							<p>4、交卷</p>
+						</div>
+					</div>
+					<div class="go">
+						<div class="ts">图片试卷</div>
+						<el-button class="buttom i">图片试卷上传答案</el-button>
+						<div class="ts1">
+							<p>1、适用于图片试卷上传答案图片</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -84,13 +99,12 @@
 				userID: localStorage.getItem('userID'),
 				userSchoolName: localStorage.getItem('userSchoolName'),
 				userGrade: localStorage.getItem('userGrade'),
-				examId:this.$route.query.id,
-				examTitle:'',
-				examParticular:'',
+				examId: this.$route.query.id,
+				examTitle: '',
+				examParticular: '',
 				papers: {},
 				classes: '',
 				dialogVisible: false,
-
 			}
 		},
 		methods: {
@@ -102,7 +116,20 @@
 			handleCurrentChange(val) {
 				this.page.pageNum = val
 				this.selectPaper();
-			}
+			},
+			// ---跳转有答题卡--
+			goScantronHas(){
+				this.$router.push({name:'examination_scantronHas',query:{id:this.examId}})
+			},
+			//---跳转无答题卡---
+			goScantronNone(){
+				this.$router.push({name:'examination_scantronNone',query:{id:this.examId}})
+			},
+			//---跳转人脸---
+			goFaceRecognition(){
+				this.$router.push({name:'examination_faceRecognition',query:{id:this.examId}})
+			},
+			//---跳转图片试卷---
 		},
 		mounted() {
 			this.loading = true,
@@ -110,11 +137,11 @@
 				apiStudentAccountSelectById(this.userID).then(res => {
 					this.classes = res.data.data.classes.name
 				})
-				// ---查询试卷---
-				apiCommonExamSelectById(this.examId).then(res=>{
-					this.examTitle=res.data.data.title
-					this.examParticular=res.data.data.examExplain
-				})
+			// ---查询试卷---
+			apiCommonExamSelectById(this.examId).then(res => {
+				this.examTitle = res.data.data.title
+				this.examParticular = res.data.data.examExplain
+			})
 		},
 	};
 </script>
--- src/components/page/examination/examination_scantronHas.vue
+++ src/components/page/examination/examination_scantronHas.vue
@@ -1,104 +1,77 @@
 <template>
 	<div class="box">
-		<div class="message-row">
-			<el-row :gutter="20" type="flex">
-				<el-col :span="8">
-					<router-link :to="{path:'manage_student'}">
-					<div class="grid-content bg-purple">
-						<div class="card-left">
-							<div class="card-c">全部试卷</div>
-							<div class="card-cc">
-								<ICountUp :endVal="total" />
-							</div>
+		<!-- 左边 -->
+		<div class="left-box">
+			<div class="box-card">
+				<div class="answer">
+					<div class="ts">距离考试结束还有</div>
+					<div class="ts-time">01:50:55</div>
+					<div class="all-topic-box">
+						<div class="at-top">作答进度:<el-progress :text-inside="true" :stroke-width="26" :percentage="percentage" :color="customColor"
+							 :format="format"></el-progress>
 						</div>
-						<div class="card-right"></div>
-					</div>
-					</router-link>
-				</el-col>
-				<el-col :span="8">
-					<router-link :to="{path:'manage_student'}">
-					<div class="grid-content bg-purple" :style="style.card_2">
-						<div class="card-left">
-							<div class="card-c">可以下载</div>
-							<div class="card-cc">
-								<ICountUp :endVal="download" />
+						<div class="li">
+							<div class="at-title">第一大题、选择题</div>
+							<div class="at-number">
+								<el-checkbox-group v-model="topicDefault" size="medium" @change="schedule">
+									<el-checkbox-button v-for="(d,i) in topic" :label="d" :key="d.i">{{d}}</el-checkbox-button>
+								</el-checkbox-group>
 							</div>
-						</div>
-						<div class="card-right" :style="style.cardRight_2"></div>
-					</div>
-					</router-link>
-				</el-col>
-				<el-col :span="8">
-					<router-link :to="{path:'manage_student'}">
-					<div class="grid-content bg-purple" :style="style.card_3">
-						<div class="card-left">
-							<div class="card-c">下载失效</div>
-							<div class="card-cc">
-								<ICountUp :endVal="disabled" />
+							<div class="at-title">第二大题、综合题</div>
+							<div class="at-number">
+								<el-checkbox-group v-model="topicDefault" size="medium" @change="schedule">
+									<el-checkbox-button v-for="(d,i) in topic2" :label="d" :key="d.i">{{d}}</el-checkbox-button>
+								</el-checkbox-group>
 							</div>
-						</div>
-						<div class="card-right" :style="style.cardRight_3"></div>
-					</div>
-					</router-link>
-				</el-col>
-			</el-row>
-		</div>
-		<div class="papers-box" v-loading="loading">
-			<div class="p-li" v-for="(d,i) in papers" :key="d.i" :style="d.status==0?(1):style.pLi" @click="downloadFile(d)" style="cursor: pointer">
-				<div class="p-icon-box">
-					<div class="p-icon"></div>
-				</div>
-				<div class="p-particula">
-					<div class="top-box">
-						<!-- <div class="subject">{{d.subject}}</div> -->
-						<div class="grade">{{d.gradeClass}}</div>
-					</div>
-					<div class="p-title">{{d.title}}</div>
-					<div class="p-time">{{d.modifyDate}}</div>
-					<div class="p-time">
-						开始下载时间：{{d.startTime}}
-					</div>
-					<div class="p-time">
-						结束下载时间：{{d.overTime}}
-					</div>
-					<div class="p-status" :style="d.status==0?(1):style.pStatus">{{d.status==0 && d.putInto == 1?'可以下载':'不允许下载'}}</div>
-					<i  class="p-status-icon el-icon-download" v-if="d.status == 0 && d.putInto == 1">
-						
-					</i>
-					<i  class="p-status-icon   el-icon-close " style="color: rgb(242, 94, 67);"   v-if="d.status == 1 || d.putInto == 0"  >
-						
-					</i>
 
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-		<!-- 分页 -->
-		<div class="page">
-			<el-pagination background layout="prev, pager, next, jumper" @size-change="handleSizeChange" @current-change="handleCurrentChange"
-			 :current-page.sync="currentPage" :page-size="pageSize" :total="total">
-			</el-pagination>
+		<!-- 右边 -->
+		<div class="right-box">
+			<div class="questions">
+				<div class="message-top">
+					<div>{{this.topicDefault[this.topicDefault.length-1]}}</div>
+					<el-tag effect="dark" class="tag">有答题卡作答</el-tag>
+				</div>
+				<div class="content-b">
+					<div class="c-title">请问唧唧复唧唧的下一句是什么？</div>
+				</div>
+				<el-radio-group v-model="choiceKey" class="choice">
+					<el-radio-button label="A">A.备选项</el-radio-button>
+					<el-radio-button label="B">B.备选项</el-radio-button>
+					<el-radio-button label="C">C.备选项</el-radio-button>
+					<el-radio-button label="D">D.备选项</el-radio-button>
+				</el-radio-group>
+				<div class="userChoice">你已选择：<span style="color: #1AAEFB;font-weight: bold;">{{choiceKey}}</span></div>
+				<div style="display: flex;justify-content: center;">
+					<el-button class="reset">清空本题答案</el-button>
+				</div>
+				<div class="message-top" style="margin-top: 10px;margin-bottom: 16px;">上传日志</div>
+				<div class="questions-img">
+					<div v-for="url in urls" :key="url" style="text-align: center;">
+						<el-image :src="url" lazy :preview-src-list="srcList" style="width: 180px;margin: 0 10px;"></el-image>
+						<span>5545</span>
+					</div>
+
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-	import Schart from 'vue-schart'
-	import bus from '../../common/bus'
-	import ICountUp from 'vue-countup-v2'
-	import {
-		handleSizeChange,
-		handleCurrentChange,
-		studentIndexData,
-		
-	} from '@/assets/js/index.js'
 	import {
 		studentIndex,
-		apicommonExamGetFile
+		apicommonExamGetFile,
+		apiStudentAccountSelectById,
+		apiCommonExamSelectById
 	} from '@/api/api.js'
 	export default {
 		name: 'index_student',
 		data() {
-			this.colors = ['#67C23A', '#409EFF', '#F56C6C']
 			return {
 				total: 0,
 				pageSize: 9,
@@ -106,124 +79,106 @@
 				currentPage: 1,
 				download: 0,
 				disabled: 0,
-				loading:false,
+				loading: false,
 				status: '',
-				style: {
-					card_2: 'background-color: #41dde3;',
-					card_3: 'background-color: #e35841;',
-					cardRight_2: {
-						backgroundImage: 'url(' + require('../../../assets/img/bg-2.png') + ')'
-					},
-					cardRight_3: {
-						backgroundImage: 'url(' + require('../../../assets/img/bg-3.png') + ')'
-					},
-					pLi: {
-						backgroundImage: 'url(' + require('../../../assets/img/fail-download.png') + ')'
-					},
-					pStatus: 'color:#e2633b'
-				},
+				examId: this.$route.query.id,
+				examTitle: '',
+				examParticular: '',
 				papers: {},
+				classes: '',
 				dialogVisible: false,
+				percentage: 0,
+				customColor: '#409eff',
+				topicDefault: [],
+				topic: ['第1题', '第2题', '第3题', '第4题', '第5题'],
+				topic2: ['第7题', '第8题', '第9题', '第10题', '第11题', '第12题', '第13题', '第14题'],
+				choiceKey: '',
+				urls: [
+					'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+					'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+					'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
+					'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
+					'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
+					'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
+					'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg'
+				],
+				srcList: [
+
+					'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+					'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+					'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
+					'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
+					'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
+					'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
+					'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg'
+				]
 			}
-		},
-		components: {
-			Schart,
-			ICountUp
 		},
 		methods: {
-			handleSizeChange,handleCurrentChange,studentIndexData
-			,
-			downloadFile(item){
-				if(item.status == 0){
-					if(item.startTime && item.overTime){
-					let arr = this.isDuringDate(item.startTime,item.overTime)
-					if(arr){
-						if(item.affix){
-							apicommonExamGetFile(item.id).then(res=>{
-								// console.log(res)
-								var headers = res.headers['content-disposition']
-								// console.log(headers)
-								headers = headers.substring(headers.indexOf('filename=\"')+'filename=\"'.length).split("\"")[0];
-								const blob = new Blob([res.data],{type:''})
-								let link = document.createElement('a');
-								let objectUrl = URL.createObjectURL(blob);
-								link.setAttribute("href",objectUrl);
-								link.setAttribute("download",headers); 
-								link.click();
-								//释放内存
-								window.URL.revokeObjectURL(link.href)
-							})
-						}else{
-							let  createTestPaperInfoObj = {
-						 		testPaperId:item.id,
-						        students:[
-						          {
-						            uid:localStorage.getItem('userID'),
-									utype:"student",
-				          			items:[]
-						          }
-						        ]
-						      }
-							this.$router.push({name :'test_paper_maker_for_task',query:{createTestPaperInfoObj:createTestPaperInfoObj}})
-						}
-						
-					}else{
-						this.$message.warning('未到下载时间，不允许下载')
-					}
-					}else{
-						if(item.affix){
-							apicommonExamGetFile(item.id).then(res=>{
-								// console.log(res)
-								var headers = res.headers['content-disposition']
-								// console.log(headers)
-								headers = headers.substring(headers.indexOf('filename=\"')+'filename=\"'.length).split("\"")[0];
-								const blob = new Blob([res.data],{type:''})
-								let link = document.createElement('a');
-								let objectUrl = URL.createObjectURL(blob);
-								link.setAttribute("href",objectUrl);
-								link.setAttribute("download",headers); 
-								link.click();
-								//释放内存
-								window.URL.revokeObjectURL(link.href)
-							})
-						}else{
-							let  createTestPaperInfoObj = {
-							 		testPaperId:item.id,
-							        students:[
-							          {
-							            uid:localStorage.getItem('userID'),
-										utype:"student",
-				          				items:[]
-							          }
-							        ]
-							      }
-							this.$router.push({name :'test_paper_maker_for_task',query:{createTestPaperInfoObj:createTestPaperInfoObj}})
-						}
-					}
-				}
+			//---分页---
+			handleSizeChange(val) {
+
 			},
-			isDuringDate(beginDateStr,endDateStr){
-				var curDate = new Date()
-				var	beginDate = new Date(beginDateStr)
-				var endDate = new Date(endDateStr)
-				
-				if(curDate >= beginDate && curDate <=endDate){
-					return true
+			//---分页2---
+			handleCurrentChange(val) {
+				this.page.pageNum = val
+				this.selectPaper();
+			},
+			format(percentage) {
+				if (percentage == 100) {
+					this.customColor = '#5cb87a'
+				} else {
+					this.customColor = '#409eff'
 				}
-				return false
-			}
+				return percentage === 100 ? '已完成' : `${percentage}%`;
+			},
+			// 计算完成度
+			schedule() {
+				let arr = this.topic.concat(this.topic2).length
+				let choice = this.topicDefault.length
+				this.percentage = parseInt(choice / arr * 100)
+			},
+			// ---跳转有答题卡--
+			goScantronHas() {
+				this.$router.push({
+					name: 'examination_scantronHas',
+					query: {
+						id: this.examId
+					}
+				})
+			},
+			//---跳转无答题卡---
+			goScantronNone() {
+				this.$router.push({
+					name: 'examination_scantronNone',
+					query: {
+						id: this.examId
+					}
+				})
+			},
+			//---跳转人脸---
+			goFaceRecognition() {
+				this.$router.push({
+					name: 'examination_faceRecognition',
+					query: {
+						id: this.examId
+					}
+				})
+			},
+			//---跳转图片试卷---
 		},
 		mounted() {
-			this.loading = true
-			let that =this;
-			// 统计数据全部数据
-			that.studentIndexData()
-			this.loading = false
+			this.loading = true,
+				// ---查询试卷---
+				apiCommonExamSelectById(this.examId).then(res => {
+					this.examTitle = res.data.data.title
+					this.examParticular = res.data.data.examExplain
+				})
 		},
 	};
 </script>
 
-<style scoped src="../../../assets/css/index.css"></style>
+<style scoped src="../../../assets/css/examination-other.css"></style>
 // 修改element 自带样式
 <style>
 	.el-pagination button:disabled {
